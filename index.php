c9f1eae6215163a9c60949f719d151e0ed4b81c1
torreshannai <torresorozcocesiahannai@gmail.com>
2024-07-17 23:17:02 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="keywords" conent="infrared, infraestructura, clínica, uttec">
+    <meta name="description" content="Preoyecto integrador de infraestructura de redes, equipo 4">
+    <meta name="author" content="Tavera, José, José, Saucedo">
+    <meta http-equiv="refresh" content="3600">
+    <meta name="viewport" content="width=device-width; initial-scale=1.0"> 
+    <link rel="stylesheet" href="css/decoracion.css">
+    <link rel="icon" href="Images/logo3.jpeg">
+    <title>Index</title>
+</head>
+    <body>
+        <nav>
+            <div class="logo"><h3>Connection<span> Point</span></h3></div>
+            <div class="color"><span style="--clr: #f96a04;"></span></div>
+            <div class="list">
+                <ul>
+                    <li><a href="interfaces/AcerProyec.php">Acerca del proyecto</a></li>
+                    <li><a href="interfaces/Ubicacion.php">Ubicación</a></li>
+                    <li><a href="pages/Esatis.php">Encuesta de satisfacción</a></li>
+                    <li><a href="pages/contacto.php">Contáctanos</a></li>
+                </ul>
+            </div>
+        </nav>
+            <div class="text">
+                <h1>Infraestructura de <span>red</span></h1>
+                <br/>
+                <h3><p id="des1" style="text-align:justify ;">La infraestructura de red en cualquier organización es crucial para garantizar una comunicación eficiente y segura entre los diferentes sistemas y dispositivos que utilizan tanto los médicos como los clientes. En este apartado veremos cómo funciona una infraestructura de red en una clínica, cuáles son sus objetivos, como está estructurada la red en una clínica, porque es fundamental que las clínicas y hospitales tenga una red en buenas condiciones, así como sus componentes. <br><br>
+                    Una infraestructura de red sólida es fundamental para el funcionamiento eficiente y seguro de los hospitales, y su diseño debe considerar tanto las necesidades actuales como las futuras.
+                </p></h3>
+                    
+            </div>
+
+            <div class="IMG">
+                
+                <img src="Images/logo3.jpeg" alt="" >
+            </div>
+
+
+
+
+    </body>
+</html>
